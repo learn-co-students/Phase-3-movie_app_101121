af5d0aeb8695616d672dcc186cd05df2f2941733
ixnp <enprocopios@gmail.com>
2021-11-24 09:49:14 -0800

    solution 03 oo pt2

--- movies_app/lib/Ticket.rb
+++ movies_app/lib/Ticket.rb
@@ -11,4 +11,20 @@ class Ticket
     def self.all
         @@all
     end 
+
+    def self.find_tickets(movie)
+        @@all.filter{|t| t.title == movie}.map{|t| "Name:#{t.name}, Price:#{t.price}"}
+    end     
+
+    def print_ticket
+        puts "Name: #{@name}"
+        puts "Movie: #{@movie}"
+        puts "Price: #{@price}"
+        puts "------------------"
+    end 
+
+    def discount
+        @price = @price/2
+        self.print_ticket
+    end 
 end 
--- movies_app/lib/cli.rb
+++ movies_app/lib/cli.rb
@@ -1,5 +1,6 @@
 #Initialize's CLI 
 def initialize_app 
+    seed
     menu
 end 
 
@@ -8,8 +9,10 @@ def menu
     puts "Welcome to Flatiron Movies"
     puts "Please choose an option:"
     puts "1. List all Movies"
-    puts "2. List Ticket holder names"
+    puts "2. List all tickets"
     puts "3. Add ticket"
+    puts "4. Find Tickets"
+    puts "5. Discount"
     puts "0. to exit"
     
     user_input = gets.strip
@@ -18,13 +21,27 @@ def menu
     when "1"
         movies = ['2001: A Space Odyssey', 'The Godfather', 'Citizen Kane', 'Raiders of the Lost Ark', 'In the mood for love', 'Seven Samurai']
         movies.each {|movie| puts movie }
-        menu
+        return_to_menu
     when "2"
-       puts Ticket.all.map{|t| t.name}
-       menu
+       puts Ticket.all.map{|t| t.print_ticket}
+       return_to_menu
     when "3"
         puts create_ticket
-        menu
+        return_to_menu
+    when "4"
+        puts "What movie are you looking for"
+        movie = gets.strip
+        puts Ticket.find_tickets(movie)
+        return_to_menu
+    when "5"
+        puts "What is your name"
+        name = gets.strip
+        puts "What Movie"
+        title = gets.strip
+
+        ticket = Ticket.all.find{|t| t.name == name && t.title == title}
+        ticket.discount
+        return_to_menu
     else 
         puts "goodbye"
     end 
@@ -38,3 +55,25 @@ def create_ticket
      t1 = Ticket.new(name, title)
 end 
 
+def return_to_menu
+    puts "Press any button to return to menu"
+    gets.strip
+    menu
+end 
+
+def seed
+    Ticket.new('rose', '2001: A Space Odyssey')
+    Ticket.new('ix','2001: A Space Odyssey' )
+    Ticket.new('bubbles', 'The Godfather')
+    Ticket.new('ix', 'The Godfather')
+    Ticket.new('jennie','The Godfather' )
+    Ticket.new('tony', 'The Godfather')
+    Ticket.new('Azula','Raiders of the Lost Ark')
+    Ticket.new('Ty Lee', 'Raiders of the Lost Ark')
+    Ticket.new('May', 'Raiders of the Lost Ark')
+    Ticket.new('Usagi', 'In the mood for love' )
+    Ticket.new('Mamoru', 'In the mood for love' )
+    Ticket.new('Alyosha', 'Citizen Kane')
+    Ticket.new('Ivan', 'Citizen Kane')
+    Ticket.new('Pavel', 'Citizen Kane')
+end 
