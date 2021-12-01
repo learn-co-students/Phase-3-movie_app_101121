ccdaa952ee58668fd6ca7d91933591d84cdb7479
ixnp <enprocopios@gmail.com>
2021-12-01 09:48:54 -0800

    starter

---
+++ movies_app/db/migrate/20211201174656_create_movies.rb
@@ -0,0 +1,10 @@
+class CreateMovies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :director
+      t.string :description
+      t.boolean :showing
+  end
+  end
+end
