7986a536acefeff92af900d7621f92c7e456314f
ixnp <enprocopios@gmail.com>
2021-12-07 09:46:04 -0800

    sinatra crud

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < Sinatra::Base
+  # By default sinatra's Content-type responce header is text/html
+  # This will set sinatra's Content-type to 'application/json'
   set :default_content_type, 'application/json'
   
   get "/" do
     { message: "Welcome to Sinatra" }.to_json
   end
+
+  get "/movies" do
+    movies = Movie.all
+    movies.to_json
+  end
+
+
 
 
 end
---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import {useState, useEffect} from 'react'
+
+function App() {
+  const [tickets, setTickets] = useState()
+
+  useEffect(()=>{
+    fetch('http://localhost:9292/tickets')
+    .then(res => res.json())
+    .then(setTickets)
+  },[])
+
+  return (
+    <div className="App">
+      {tickets.map(t => {
+        return(
+          <div>
+          <h1>{t.name}</h1>
+          <p>{t.price}</p>
+          <p>{t.movie.title}</p>
+          <button>Delete Ticket</button>
+          </div>
+        )
+      })}
+    </div>
+  );
+}
+
+export default App;
